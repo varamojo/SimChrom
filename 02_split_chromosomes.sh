b7c48ec330a8ac289b12870ec2665f60fe008a2f
Ilia Varamogianni <74252145+varamojo@users.noreply.github.com>
2024-10-29 12:30:47 +0100

    Update 02_split_chromosomes.sh

--- 02_split_chromosomes.sh
+++ 02_split_chromosomes.sh
@@ -1,6 +1,6 @@
 #Set parameters
 cmm_Dir="home/path_to_cmm"                                      #output directory to the Chrom3D output (.cmm file)
-cmm_File="3M_model_ladatlas.CTCF.cmm"                           #output file name (.cmm)
+cmm_File="3M_model_ladatlas.CTCF.cmm"                           #output file name with CTCF highlight (.cmm)
 
 #Split chromosome parameters
 declare -a chrom=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX)
